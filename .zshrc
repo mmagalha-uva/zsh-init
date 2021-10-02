c2d7bef862fcbc71053695f51ce25c323633f45b
mmagalha <marcos.magalhaes@urbanovitalino.com.br>
2021-10-02 08:33:57 -0300

    change output

--- .zshrc
+++ .zshrc
@@ -9,7 +9,7 @@ fi
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="~/.oh-my-zsh"
+export ZSH="/$USENAME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
@@ -85,7 +85,7 @@ plugins=(
 	zsh-autosuggestions
 	zsh-syntax-highlighting
 	zsh-history-substring-search
-        docker
+    docker
 )
 
 source $ZSH/oh-my-zsh.sh
--- zsh-init.sh
+++ zsh-init.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
+
 echo "Install inital packages"
-sudo dnf install -y \
-git \
-curl \
-python3-pip \
-zsh \
-util-linux-user
+{
+    sudo dnf install -y \
+    git \
+    curl \
+    python3-pip \
+    zsh \
+    util-linux-user
+} 2>&1 | tee /tmp/outputs.log
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
+echo "Install zsh customizations"
+{
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    curl -Lo ~/.local/share/fonts/MesloLGS_NF_Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+    curl -Lo ~/.p10k.zsh https://raw.githubusercontent.com/mmagalha-uva/zsh-init/main/.p10k.zsh
+    curl -Lo ~/.zshrc https://raw.githubusercontent.com/mmagalha-uva/zsh-init/main/.zshrc
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+    sudo chsh -s /usr/bin/zsh $USERNAME
+} 2>&1 | tee /tmp/outputs.log
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "Install custom packages"
 
-curl -Lo ~/.local/share/fonts/MesloLGS_NF_Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+echo "Install terraform"
 
-curl -Lo ~/.p10k.zsh https://raw.githubusercontent.com/mmagalha-uva/zsh-init/main/.p10k.zsh
-
-curl -Lo ~/.zshrc https://raw.githubusercontent.com/mmagalha-uva/zsh-init/main/.zshrc
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-
-sudo chsh -s /usr/bin/zsh $USERNAME
 
