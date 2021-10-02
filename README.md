45c17c33cbe01c31c979ed289a15bfcebf5371e0
mmagalha <marcos.magalhaes@urbanovitalino.com.br>
2021-10-02 07:28:07 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## zsh-init
+
+initialize zsh with oh-my-zsh and powerlevel10k theme
+
+usage:
+
+sh -c "$(curl -fsSL https://raw.github.com/mmagalha/zsh-init/master/tools/install.sh)"
---
+++ zsh-init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "Install inital packages"
+sudo dnf install -y 
+git \
+curl \
+terraform \
+python3-pip \
+zsh \
+util-linux-user
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+curl -Lo ~/.local/share/fonts/MesloLGS\ NF\ Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+
+
+
+
